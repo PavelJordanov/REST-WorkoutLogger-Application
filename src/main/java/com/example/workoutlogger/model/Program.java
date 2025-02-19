063dde474bf06dfee7e0b2f1597bb5e6a5f8dce2
paj4 <paj4@sfu.ca>
2025-02-19 14:56:33 -0800

    added new entity program model file

---
+++ src/main/java/com/example/workoutlogger/model/Program.java
@@ -0,0 +1,67 @@
+package com.example.workoutlogger.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    private String id;
+    private String name;
+    private Boolean done;
+
+    List<Week> weeks = new ArrayList<>();
+
+    public Program() {}
+
+    public Program(String id, String name, Boolean done) {
+        this.id = id;
+        this.name = name;
+        this.done = false;
+    }
+
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getDone() {
+        return this.done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    public List<Week> getWeeks() {
+        return this.weeks;
+    }
+
+    public void setWeeks(List<Week> weeks) {
+        this.weeks = weeks;
+    }
+
+    public boolean canBeMarkedAsDone() {
+        return weeks.stream().allMatch(Week::getDone);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", done='" + getDone() + "'" +
+            ", weeks='" + getWeeks() + "'" +
+            "}";
+    }
+}
