19f0ae1706fb90d2dbacac9b58e044563bdc2003
paj4 <paj4@sfu.ca>
2025-02-25 18:40:24 -0800

    Added new entities to seperately track the status of the entities for a user, ensuring at the same time that the exercise, workout etc. classes are only definitions, will expand in the next push

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,6 @@
--- Insert exercises next
 INSERT INTO exercise (id, name, sets, reps, weight, done) VALUES ('e1', 'Squats', 4, 12, 100, FALSE);
 INSERT INTO exercise (id, name, sets, reps, weight, done) VALUES ('e2', 'Bench Press', 4, 10, 150, FALSE);
 
--- Insert workouts first
 INSERT INTO workout (id, name, done) VALUES ('w1', 'Leg Day', FALSE);
 INSERT INTO workout (id, name, done) VALUES ('w2', 'Upper Body', FALSE);
 
